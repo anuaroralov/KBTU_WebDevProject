6cf18b79981f9997135904a00c178455e5e60a61
Kaiyrzhan <seitkarim11@gmail.com>
2024-04-18 01:35:04 +0500

    backend and logo added

--- frontend/README.md
+++ frontend/README.md
@@ -1,6 +1,6 @@
-# WebDevProject
+# Frontend
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.2.0.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.1.2.
 
 ## Development server
 
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,12 +13,13 @@ import { FooterComponent } from './layouts/footer/footer.component';
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatSlideToggleModule
+    MatSlideToggleModule,
+    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
