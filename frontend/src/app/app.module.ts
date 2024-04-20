e7b148452854796d9e304f9b2de82e64c0cd85a2
Anuar Oralov <anuar.oralov@gmail.com>
2024-04-20 15:56:52 +0500

    some changes

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,20 +5,22 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { HeaderComponent } from './layouts/header/header.component';
 import { FooterComponent } from './layouts/footer/footer.component';
+import { ButtonComponent } from './components/button/button.component';
+import { InputComponent } from './components/input/input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
+    ButtonComponent,
+    InputComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatSlideToggleModule,
     FontAwesomeModule,
   ],
   providers: [],
